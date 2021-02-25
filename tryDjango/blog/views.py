ba7e832a6c89a01700ded806e5cf302f043dc488
blackjohnrose <18.mamali@gmail.com>
2021-02-25 18:06:53 +0330

    add smple list of data

--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+post = [
+    {
+        'auther': 'tryDjango',
+        'title': 'Blog post 1',
+        'content': 'the first content',
+        'date_post': 'Feb 25, 2021 18:03:35'
+    },
+        {
+        'auther': 'tryDjango 2',
+        'title': 'Blog post 2',
+        'content': 'the second content',
+        'date_post': 'Feb 25, 2021 18:04:22'
+    }
+]
+
 
 def home_view(request):
+    context = {
+        "post": post
+    }
 
-    return render(request, "blog/blog_home.html")
+    return render(request, "blog/blog_home.html", post)
 
 
 def about_view(request):
