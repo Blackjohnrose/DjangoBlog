cd70859adcd281556cd57384dbbbe8b484f2c035
blackjohnrose <18.mamali@gmail.com>
2021-02-25 20:16:42 +0330

    edit smple data

--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 post = [
     {
-        'auther': 'tryDjango',
+        'author': 'tryDjango',
         'title': 'Blog post 1',
         'content': 'the first content',
-        'date_post': 'Feb 25, 2021 18:03:35'
+        'date_posted': 'Feb 25, 2021 18:03:35'
     },
         {
-        'auther': 'tryDjango 2',
+        'author': 'tryDjango 2',
         'title': 'Blog post 2',
         'content': 'the second content',
-        'date_post': 'Feb 25, 2021 18:04:22'
+        'date_posted': 'Feb 25, 2021 18:05:35'
     }
 ]
 
