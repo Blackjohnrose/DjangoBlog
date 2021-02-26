8c835391c5cc236a204b1a3c17a3bb215585f3f9
blackjohnrose <18.mamali@gmail.com>
2021-02-26 13:48:48 +0330

    delete dummy or simple date

--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-post = [
-    {
-        'author': 'tryDjango',
-        'title': 'Blog post 1',
-        'content': 'the first content',
-        'date_posted': 'Feb 25, 2021 18:03:35'
-    },
-        {
-        'author': 'tryDjango 2',
-        'title': 'Blog post 2',
-        'content': 'the second content',
-        'date_posted': 'Feb 25, 2021 18:05:35'
-    }
-]
-
-
 def home_view(request):
     post = Post.objects.all()
     context = {
