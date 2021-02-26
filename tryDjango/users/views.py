9e1f5bbd8711c7bcf164588cf0cc11b77c6efcbe
blackjohnrose <18.mamali@gmail.com>
2021-02-26 16:48:23 +0330

    add  fun register_view for render forms

--- tryDjango/users/views.py
+++ tryDjango/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register_view(request):
+    form = UserCreationForm()
+    context = [
+        'form': form,
+        'title': 'register'
+    ]
+    return render(request, 'users/register.html')
