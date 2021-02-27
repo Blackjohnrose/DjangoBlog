8d9daefa692f92c01bdc200e2ef358802d1f7daa
blackjohnrose <18.mamali@gmail.com>
2021-02-27 17:54:47 +0330

    import LoginView and fix urls after at add Logout path

--- tryDjango/tryDjango/urls.py
+++ tryDjango/tryDjango/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from users.views import register_view
 
 urlpatterns = [
-	path('', include('blog.urls')),
-    path('register/', register_view, name='register'),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register'),
+
 ]
