51335d42b5328907dece93cd8656c52c2930ed0d
blackjohnrose <18.mamali@gmail.com>
2021-02-28 23:34:46 +0330

    congif to resize image

--- tryDjango/users/views.py
+++ tryDjango/users/views.py
@@ -20,8 +20,20 @@ def register_view(request):
 
 @login_required
 def profile_view(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profileuser)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profileuser)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'You account has been updated! ')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profileuser)
 
     context = {
         'u_form': u_form,
