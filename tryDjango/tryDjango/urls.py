da4451952513143899db1ce3c2287f3b54f4cef6
blackjohnrose <18.mamali@gmail.com>
2021-02-28 16:17:51 +0330

    add media url and root inurls

--- tryDjango/tryDjango/urls.py
+++ tryDjango/tryDjango/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from users.views import register_view, profile_view
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
