983cb76f425c82663841cff1d73722555bc8f6f3
blackjohnrose <18.mamali@gmail.com>
2021-03-01 12:05:51 +0330

    add listPostView and ordering

--- tryDjango/blog/urls.py
+++ tryDjango/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home_view, about_view
+from .views import home_view, about_view, PostListView
 
 urlpatterns = [
-    path('', home_view, name="blog-home"),
+    path('', PostListView.as_view() , name="blog-home"),
     path('about/', about_view, name="blog-about"),
 
 ]
--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home_view(request):
@@ -10,6 +11,14 @@ def home_view(request):
     }
 
     return render(request, "blog/blog_home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog_home.html'
+    context_object_name = 'post'
+    ordering = ['-date_posted']
+
 
 
 def about_view(request):
