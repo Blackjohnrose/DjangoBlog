cb60c01634dbfde4f55cddc3bdc47df2b7fce735
blackjohnrose <18.mamali@gmail.com>
2021-03-01 12:50:09 +0330

    git add urls detail and crete view

--- tryDjango/blog/urls.py
+++ tryDjango/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import home_view, about_view, PostListView
+from .views import (
+    home_view,
+    about_view,
+    PostCreateView,
+    PostListView,
+    PostDetailView,
+)
 
 urlpatterns = [
-    path('', PostListView.as_view() , name="blog-home"),
     path('about/', about_view, name="blog-about"),
-
+    path('', PostListView.as_view() , name="blog-home"),
+    path('post/new/', PostCreateView.as_view() , name="post-create"),
+    path('post/<int:pk>/', PostDetailView.as_view() , name="post-detail"),
 ]
