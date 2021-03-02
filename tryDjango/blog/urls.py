5c236710129a70f0d83dd102b3c47a14128f356b
blackjohnrose <18.mamali@gmail.com>
2021-03-02 19:07:47 +0330

    add links UserPostListVew

--- tryDjango/blog/urls.py
+++ tryDjango/blog/urls.py
@@ -6,12 +6,14 @@ from .views import (
     PostCreateView,
     PostDetailView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     path('about/', about_view, name="blog-about"),
     path('', PostListView.as_view() , name="blog-home"),
+    path('user/<str:username>/', UserPostListView.as_view() , name="user-post"),
     path('post/new/', PostCreateView.as_view() , name="post-create"),
     path('post/<int:pk>/', PostDetailView.as_view() , name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view() , name="post-update"),
