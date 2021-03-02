f231e3ff3ce12802880e0c9653cd7883f6d0f59d
blackjohnrose <18.mamali@gmail.com>
2021-03-02 17:38:52 +0330

    add pagination

--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -26,6 +26,7 @@ class PostListView(ListView):
     template_name = 'blog/blog_home.html'
     context_object_name = 'post'
     ordering = ['-date_posted']
+    paginate_by = 3
 
 
 class PostDetailView(DetailView):
