9d9c2471de1c9b6f78ef2cd30cc1e291a7b6c9db
blackjohnrose <18.mamali@gmail.com>
2021-03-02 19:08:42 +0330

    add UserPostListView

--- tryDjango/blog/views.py
+++ tryDjango/blog/views.py
@@ -30,6 +30,17 @@ class PostListView(ListView):
     paginate_by = 6
 
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_post.html'
+    context_object_name = 'post'
+    paginate_by = 6
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 class PostDetailView(DetailView):
     model = Post
 
